f64ec4f1f706989cbb009d2eef6f0d38d69f8073
Jegis9 <ixcaquic22@gmailcom>
2024-03-05 18:43:49 -0600

    funcional, falta editar

--- task/urls.py
+++ task/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import list_task, create_task, delete_task
+from .views import list_task, create_task, delete_task, cont
+
 urlpatterns = [
-
-    path('',list_task),
-    path('new/',create_task),
-
-    path('delete_task/<int:task_id>/',delete_task, name='delete_task'),
-
+    path('', list_task),
+    path('new/', create_task),
+    path('delete_task/<int:task_id>/', delete_task, name='delete_task'),
+    path('cont/', cont),
+    path('report/', cont, name='report_page'),  # Agregar esta línea para la página de informes
 
 ]
-
--- task/views.py
+++ task/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
-
+from datetime import datetime
 from .models import Task
+from django.http import JsonResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
+
 
 # Create your views here.
 def list_task(request):
@@ -23,7 +27,6 @@ def create_task(request):
  
 
 
-
 def delete_task(request, task_id):
     task = Task.objects.get(id=task_id)
     task.delete()
@@ -31,4 +34,23 @@ def delete_task(request, task_id):
     
 
 
+
+
+# Create your views here.
+def cont(request):
+
+
+    tasks = Task.objects.all()
+    count = tasks.count()  # Contar la cantidad total de registros
+
+    # Contar registros mayores y menores de 18 años
+    mayor_count = 0
+    menor_count = 0
+    for task in tasks:
+        if (datetime.now().year - task.fechaNacimiento.year) >= 18:
+            mayor_count += 1
+        else:
+            menor_count += 1
+
+    return render(request, 'report.html', { 'count': count, 'mayor_count': mayor_count, 'menor_count': menor_count})
 
